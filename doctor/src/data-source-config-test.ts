087f404d4453aa257ad6285331d95cb361c13150
pranavnan <pranavnan612@gmail.com>
2025-03-06 15:36:05 +0530

    test7

--- doctor/src/data-source-config-test.ts
+++ doctor/src/data-source-config-test.ts
@@ -7,7 +7,7 @@ const TestDataSource: DataSourceOptions = {
   port: 3306,
   username: 'root',
   password: '',
-  database: 'healthcare',
+  database: 'testdb',
   synchronize: true, // Set this to false in production
   // entities: ['./entities/**/*.entity.ts'],
   entities: ['src/entities/*{.ts,.js}'], // always give the path from root of our project
